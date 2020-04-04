48a1c84023835d6bc671e86528ffb5b7ee7f8aca
unknown <snowliniker@gmail.com>
2020-04-03 23:24:24 -0300

    Cuidando de responsividade

--- main.js
+++ main.js
@@ -66,6 +66,9 @@
 
 function modal(){
     
+    var desfoque = document.getElementById('desfoque');
+    desfoque.classList.toggle('active');
+
     var popup = document.getElementById('popup');
     popup.classList.toggle('active');
 }
